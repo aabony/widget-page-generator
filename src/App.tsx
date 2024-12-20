0a70389658c7fc71dcce89a6603a785c0ec52b5b
Belymenko Dmytro <belymenko@zore.digital>
2024-12-20 18:44:19 +0200

    remove page generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { Navigation } from './components/Navigation';
-import { HeroSection } from './components/HeroSection';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {PageGenerator} from "./pages/PageGenerator.tsx";
 import {Home} from "./pages/Home.tsx"
 
 export function App() {
@@ -10,7 +7,6 @@ export function App() {
           <Router>
               <Routes>
                   <Route path="/" element={<Home />} />
-                  <Route path="/page-generator" element={<PageGenerator />} />
               </Routes>
           </Router>
       </>
