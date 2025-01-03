712e123c6893550ea22a160b663ea3cc71138b1b
Belymenko Dmytro <belymenko@zore.digital>
2025-01-03 02:30:27 +0200

    little update

--- components/LandingPageGenerator.tsx
+++ components/LandingPageGenerator.tsx
@@ -99,6 +99,31 @@ export default function LandingPageGenerator({ initialConfig, isEditing = false 
     .card-content a:hover {
       background: #0056b3;
     }
+       .social-links-wrapper {
+          background: var(--social-links-background, #3498db);
+          padding: 20px 0;
+          text-align: center;
+        }
+        .social-links {
+          display: flex;
+          justify-content: center;
+          gap: 20px;
+        }
+        .social-links a {
+          font-size: 24px;
+          color: white;
+          background: rgba(255, 255, 255, 0.2);
+          width: 50px;
+          height: 50px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 50%;
+          transition: all 0.3s ease;
+        }
+        .social-links a:hover {
+          background: rgba(255, 255, 255, 0.4);
+        }
   </style>
 </head>
 <body>
@@ -106,6 +131,31 @@ export default function LandingPageGenerator({ initialConfig, isEditing = false 
     <h1>${pageConfig.title}</h1>
     <p>${pageConfig.subtitle}</p>
   </header>
+    <div class="social-links-wrapper">
+    <div class="social-links">
+      ${pageConfig.socialLinks
+        .map(
+            (link) => `
+          <a href="${link.url}" target="_blank" rel="noopener noreferrer">
+            ${
+                link.platform === 'facebook'
+                    ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 50 50">
+                    <path d="M 25 3 C 12.861562 3 3 12.861562 3 25 C 3 36.019135 11.127533 45.138355 21.712891 46.728516 L 22.861328 46.902344 L 22.861328 29.566406 L 17.664062 29.566406 L 17.664062 26.046875 L 22.861328 26.046875 L 22.861328 21.373047 C 22.861328 18.494965 23.551973 16.599417 24.695312 15.410156 C 25.838652 14.220896 27.528004 13.621094 29.878906 13.621094 C 31.758714 13.621094 32.490022 13.734993 33.185547 13.820312 L 33.185547 16.701172 L 30.738281 16.701172 C 29.349697 16.701172 28.210449 17.475903 27.619141 18.507812 C 27.027832 19.539724 26.84375 20.771816 26.84375 22.027344 L 26.84375 26.044922 L 32.966797 26.044922 L 32.421875 29.564453 L 26.84375 29.564453 L 26.84375 46.929688 L 27.978516 46.775391 C 38.71434 45.319366 47 36.126845 47 25 C 47 12.861562 37.138438 3 25 3 z"></path>
+                  </svg>`
+                    : ''
+            }
+            ${
+                link.platform === 'instagram'
+                    ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 50 50">
+                    <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z"></path>
+                  </svg>`
+                    : ''
+            }
+          </a>`
+        )
+        .join('')}
+    </div>
+  </div>
   <div class="attention-ribbon">${pageConfig.ribbonText}</div>
   <div class="card-container">
     ${pageConfig.offers
