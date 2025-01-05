c03dc6f685c915f16d8db10e91f3c9613648e129
Belymenko Dmytro <belymenko@zore.digital>
2025-01-05 18:05:51 +0200

    fix bug with picture

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   ignoreBuildErrors: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
